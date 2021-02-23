768e5699ebc60664ad7b2d68ae629a96e9446144
Arvin Xu <arvinx@foxmail.com>
2021-02-23 21:10:34 +0800

    fix: fix antd svg icon position offset bug #5

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,11 @@ export const tailwindcssContent = `/* purgecss start ignore */
 /* purgecss end ignore */
 
 @tailwind utilities;
+
+// 用于修正 antd svg icon 位置向下偏移的 bug
+svg {
+  vertical-align: initial;
+}
 `;
 
 export const tailwindConfigJS = `module.exports = {
